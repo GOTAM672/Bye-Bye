4c338777491f5018894d349a3f4dff48323af539
Gotam Gorabh <gautamy672@gmail.com>
2022-10-19 01:58:58 +0530

    adding img folder..

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+//enum GtkWindowPosition;
 
 static void
 activate (GtkApplication* app,
@@ -9,8 +10,9 @@ activate (GtkApplication* app,
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "ByeBye");
   gtk_window_set_default_size (GTK_WINDOW (window), 750, 225);
-  gtk_window_set_resizable (GTK_WINDOW(window), false);
-  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER_ON_PARENT);
+  gtk_window_set_resizable (GTK_WINDOW (window), false);
+  //gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER_ALWAYS);
+  //gtk_window_set_decorated (GTK_WINDOW (window), false);
   gtk_widget_show (window);
 }
 
